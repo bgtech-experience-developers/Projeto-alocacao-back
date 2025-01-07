65cf658770b320bf432e37f0f559ba2b37a6a8d3
Ezequias Bernardo <ezequiasobernardo@outlook.com>
2025-01-07 19:15:55 -0300

    feat: compilando o codigo

--- dist/routers/school.js
+++ dist/routers/school.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 export const schoolRouter = Router();
 schoolRouter.get('/', (req, res) => {
-    console.log('hello world');
     res.send('ping pong');
 });
 //# sourceMappingURL=school.js.map
