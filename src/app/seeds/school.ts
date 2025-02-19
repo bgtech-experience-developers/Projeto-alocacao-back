e5ea2756920c14a63b9117f4883cdf6cfe478485
Levy <85988919231levy@gmail.com>
2025-02-19 11:23:16 -0300

    feat(seeds): Criando dados fakes para produto

--- src/app/seeds/school.ts
+++ src/app/seeds/school.ts
@@ -1,17 +1,20 @@
 import {faker} from '@faker-js/faker';
-import { InstanciaPrismas } from '../connection/InstanciaPrisma';
+import  {InstanciaPrismas} from '../connection/InstanciaPrisma.js';
+import { PrismaClient } from "@prisma/client";
 
-const connectionDb = InstanciaPrismas.createConnection();
+const connectionDb: PrismaClient = InstanciaPrismas.createConnection();
 async function schoolSeeds() {
     let dados = []
     let pcd = 0
     for(let i = 0; i <= 100; i++) {
+        // console.log((faker.commerce.isbn({variant: 13, separator: ''}) + i).toString())
+        console.log(faker.string.numeric({length: {max: 4,min: 2}}))
         dados.push(
             await connectionDb.school.create({
                 data: {
                     name_school: faker.company.name(),
-                    cnpj: (faker.commerce.isbn({variant: 13, separator: ' '}) + i).toString(),
-                    answerable_school: faker.person.firstName() + " " + faker.person.lastName(),
+                    cnpj: (faker.commerce.isbn({variant: 13, separator: ''}) + 6 ).toString(),
+                    answerable_school: faker.person.firstName() + "" + faker.person.lastName(),
                     answerable_email: faker.internet.email(),
                     answerable_phone: faker.phone.number({
                         style: 'national'
@@ -19,9 +22,9 @@ async function schoolSeeds() {
                     class_room: {
                         create: {
                             floor: i.toString(),
-                            number_class: faker.airline.flightNumber(),
+                            number_class: faker.string.numeric(5),
                             amount_chair: faker.airline.flightNumber({length: 2}),
-                            type_chair: i == pcd ? "PCD" : "Comum",
+                            type_chair: i == pcd ? "PCD" : "COMUM",
                         }
                     },
                     school_address: {
